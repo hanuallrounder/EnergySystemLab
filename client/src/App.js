b92d2c5be9f83c9323875d90a0ac59985f5b2e78
Hanuman Prasad Meena <hanuallrounder@gmail.com>
2022-09-27 17:54:08 +0530

    project init

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Landing from './components/Landing';
+function App() {
+  return (
+    
+      <Fragment>
+        <Router>
+          
+            <Routes>
+              <Route exact path='/' element={<Landing />} />
+            </Routes>
+          
+        </Router>
+      </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Navbar from "./Navbar";
+function Landing() {
+    return <div>
+        <Navbar/>
+        <h1>heo</h1>
+    </div>
+} 
+export default Landing;
